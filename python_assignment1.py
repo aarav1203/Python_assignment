ab141c2869f2dbb29f4617660907db2e99d25948
AARAV KUMAR SINGH <62836744+aarav1203@users.noreply.github.com>
2022-11-15 17:18:21 +0530

    Add files via upload

---
+++ python_assignment1.py
@@ -0,0 +1,31 @@
+#  1.Write a Python program to print "Hello Python"?
+print("Hello Python")
+
+# 2.Write a Python program to do arithmetical operations addition and division.?
+a=10
+b=5
+#Addition
+add=a+b
+##Division
+div=a/b
+print(add)
+print(div)
+
+# 3. Write a Python program to find the area of a triangle?
+height=100
+base=25
+area=0.5*(base*height)
+print(area)
+
+# 4.Write a Python program to swap two variables?
+a=5
+b=10
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+
+# 5.Write a Python program to generate a random number?
+import random
+print(random.randint(1,50))
